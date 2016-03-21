4ef658c94538b66716ad143a416f2e1772f4e0a9
Darragh O'Toole <darragh.otoole@gmail.com>
2016-03-21 14:15:53 +0000

    Added mongodb-master image, removed old mongo files

--- images/mongodb-master/setup.sh
+++ images/mongodb-master/setup.sh
@@ -1,14 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 echo "Waiting for startup.."
 
 init_mongo () {
   mongo "localhost:27017" <<EOF
     rs.initiate();
+    rs.slaveOk();
 EOF
   return $?
 }
 
 until init_mongo; do
-  printf '.'
   sleep 1
 done
+
+echo "Mongo configured!"
